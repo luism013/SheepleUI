e0056397e9fba832a4970eaebfce053ac1dc1f89
lm_013 <luis.rivera93@upr.edu>
2019-05-09 14:31:20 -0700

    login and register should work :).
    now to do logout

--- appjs/login.js
+++ appjs/login.js
@@ -42,11 +42,11 @@ angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scop
             $log.error("Users Loaded: ", JSON.stringify());
         };
 
-        this.register = function(firstname, lastname, email, phone, username, password){
+        this.register = function(firstname, lastname, email, phone, gender, username, password){
             var reqURL = "http://localhost:5000/Sheeple/register";
             console.log("reqURL: " + reqURL);
             var data = {'first_name': firstname, 'last_name': lastname, 'email': email,
-                'phone': phone, 'username': username, 'password': password};
+                'phone': phone, 'username': username, 'password': password, 'gender': gender};
             // Now issue the http request to the rest API
             $http.post(reqURL, data).then(
                 // Success function
