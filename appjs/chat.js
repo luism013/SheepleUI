c44f8be685ca80926a883f2e7e4d370a74a01706
Zernin <jonathan.flechas@upr.edu>
2019-05-16 18:47:18 -0700

    Finished Chat.js and Chat.html

--- appjs/chat.js
+++ appjs/chat.js
@@ -3,17 +3,16 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
         "use strict";
         var thisCtrl = this;
 
+        this.repliesList = []
         this.messageList = [];
         this.chatsList = [];
         this.likesList = [];
         this.dislikesList = [];
         this.currentUser = currUser.getUser();
         this.currentChat = {};
-        this.searchList = [];
-        this.currentUsers = []
+        this.currentUsers = [];
 
-        this.openChat = true;
-        this.openSearch = true;
+        this.replies = false;
 
         this.loadChats = function(){
             // Now create the url with the route to talk with the rest API
@@ -103,6 +102,48 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             $log.error("Message Loaded Load Messages: ", JSON.stringify(thisCtrl.messageList));
         };
 
+        this.loadReplies = function(post_id){
+            thisCtrl.currPost(post_id)
+            thisCtrl.replies = true;
+            console.log(post_id)
+            // Now create the url with the route to talk with the rest API
+            var reqURL = "http://localhost:5000/Sheeple/posts/replies/" + post_id;
+            console.log("reqURL: " + reqURL);
+            // Now issue the http request to the rest API
+            $http.get(reqURL).then(
+                // Success function
+                function (response) {
+                    console.log("data: " + JSON.stringify(response.data));
+                    // assing the part details to the variable in the controller
+
+                        thisCtrl.repliesList = response.data.Replies;
+                   // thisCtrl.repliesList = response.data.Replies;
+                },
+                function (response){
+                    // This is the error function
+                    // If we get here, some error occurred.
+                    // Verify which was the cause and show an alert.
+                    var status = response.status;
+                    if (status == 0){
+                        alert("No hay conexion a Internet");
+                    }
+                    else if (status == 401){
+                        alert("Su sesion expiro. Conectese de nuevo.");
+                    }
+                    else if (status == 403){
+                        alert("No esta autorizado a usar el sistema.");
+                    }
+                    else if (status == 404){
+
+                    }
+                    else {
+                        alert("Error interno del sistema.");
+                    }
+                });
+
+            $log.error("Replies Loaded: ", JSON.stringify(thisCtrl.repliesList));
+        };
+
         this.loadWhoLiked = function(post_id){
             // Now create the url with the route to talk with the rest API
             var reqURL1 = "http://localhost:5000/Sheeple/posts/" + post_id +"/likes";
@@ -187,9 +228,52 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             $log.error("Message Loaded Load who disliked: ", JSON.stringify(thisCtrl.reactList));
         };
 
+        this.postReply = function(newText){
+            var original = localStorage.getItem('currentPost');
+            var reqURL = "http://localhost:5000/Sheeple/posts/reply";
+            console.log("reqURL: " + reqURL);
+            var d = new Date();
+            var day = d.getDate();
+            var month = d.getMonth() + 1;
+            var year  =  d.getFullYear();
+
+            var data = {'post_content': newText, 'user_id': thisCtrl.currentUser.user_id,
+                'gc_id': localStorage.getItem('currentChat'), 'username': thisCtrl.currentUser.username,
+                'image_url': 'To be fixed', 'post_date': month + "/" + day + "/" + year, 'original_post' : original };
+            console.log(data);
+            // Now issue the http request to the rest API
+            $http.post(reqURL, data).then(
+                // Success function
+                function (response) {
+                    console.log("data: " + JSON.stringify(response.data));
+                    //$location.path('/user/gchats');
+                },
+                function (response){
+                    // This is the error function
+                    // If we get here, some error occurred.
+                    // Verify which was the cause and show an alert.
+                    var status = response.status;
+                    if (status == 0){
+                        alert("No hay conexion a Internet");
+                    }
+                    else if (status == 401){
+                        alert("Su sesion expiro. Conectese de nuevo.");
+                    }
+                    else if (status == 403){
+                        alert("No esta autorizado a usar el sistema.");
+                    }
+                    else if (status == 404){
+                        alert("No se encontro la informacion solicitada.");
+                    }
+                    else {
+                        alert("Error interno del sistema.");
+                    }
+                });
+
+            $log.error("Users Loaded post message: ", JSON.stringify());
+        };
+
         this.postMsg = function(newText){
-
-
             var reqURL = "http://localhost:5000/Sheeple/post";
             console.log("reqURL: " + reqURL);
             var d = new Date();
@@ -352,6 +436,11 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
         this.currChat = function(gc_id) {
             localStorage.setItem('currentChat', gc_id);
         };
+
+        this.currPost = function(post_id) {
+            localStorage.setItem('currentPost', post_id);
+        };
+
 
         this.showContacts = function() {
             $location.path('/user/contacts');
